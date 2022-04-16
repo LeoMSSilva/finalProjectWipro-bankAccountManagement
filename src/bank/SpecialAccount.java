c2a8358702f18d4a63d50b818306d48d733bfbd8
LeoMSSilva <l.eom1994@hotmail.com>
2022-04-15 22:17:14 -0300

    Fix: fixed in moneyWithdraw method logic

--- src/bank/SpecialAccount.java
+++ src/bank/SpecialAccount.java
@@ -30,7 +30,7 @@ public class SpecialAccount extends Account {
 
 	@Override
 	public void moneyWithdraw(double value, Account account) {
-		if (this.balance + limit <= value) {
+		if (value <= (this.balance > 0 ? this.balance : this.balance + limit)) {
 			this.balance -= value;
 			System.out.printf("\nWithdrawal was successful!\nThe account balance is: $ %.2f\n", this.balance);
 			String message = String.format("\nWithdrawal from:\t$ %.2f.\tCurrent balance:\t$ %.2f", value,
